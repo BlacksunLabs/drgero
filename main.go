6ca18de98a462423ad35186a545e35ffc0ae646a
n0ncetonic <n0ncetonic@users.noreply.github.com>
2018-12-21 19:25:10 -0800

    Actually fixes syntax error...

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/BlacksunLabs/drgero/event"
 	"github.com/BlacksunLabs/drgero/mq"
@@ -28,7 +29,13 @@ func eventPOST(c *gin.Context) {
 	event.Host = host
 	event.UserAgent = ua
 
-	err = m.PublishJSONToFanoutExchange(json.Marshal(event), "events")
+	eventBody, err := json.Marshal(event)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": fmt.Sprintf("failed to marshal event into JSON: %v", err),
+		})
+	}
+	err = m.PublishJSONToFanoutExchange(eventBody, "events")
 	if err != nil {
 		c.JSON(500, gin.H{
 			"error": "Failed to publish event to events exchange",
